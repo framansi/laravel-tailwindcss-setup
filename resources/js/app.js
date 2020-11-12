87c3424a57ddc9125a6530901def9ea32192736d
Francesco <fra.mansi@gmail.com>
2020-11-12 15:31:37 +0100

    Vue 2.6.11

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,9 @@
 import Vue from 'vue';
 
+Vue.config.productionTip = false;
+
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 const app = new Vue({
     el: '#app',
