4e26c8774e3c124172f614458ebf6828ea5e513a
Francesco <fra.mansi@gmail.com>
2020-11-12 15:24:11 +0100

    Laravel 8 + Tailwind CSS 1.9

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+ module.exports = {
+     future: {},
+     purge: [
+         './storage/framework/views/*.php',
+         './resources/views/**/*.blade.php',
+     ],
+     theme: {
+         extend: {},
+     },
+     variants: {},
+     plugins: [],
+ }
